1a8aa4eb85c78dd46e13dc984b28a5fdd5ab972c
Alizamankhan786 <az030366@gmail.com>
2024-10-04 15:53:19 -0700

    Project setup Completed

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+  </div>
+  <div className="navbar-center">
+    <a className="font-bold text-xl">MEME-APP!</a>
+  </div>
+  <div className="navbar-end">
+  </div>
+</div>
+  )
+}
+
+export default Navbar;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1 className='text-center mt-5 '>APP.TSX</h1>
+  )
+}
+
+export default page
